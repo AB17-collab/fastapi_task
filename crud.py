0e6c6241b0e9068f194a72bb44a83e4e763b751e
Arnab Deb <iamab6742@gmail.com>
2026-02-03 02:45:02 +0530

    CRUD operations updated

--- crud.py
+++ crud.py
@@ -35,3 +35,28 @@ def create_book(book: Book):
     new_book = book.model_dump()
     books.append(new_book)
     return new_book
+
+
+class BookUpdate(BaseModel):
+    id: int | None = None
+    title: str | None = None
+    author: str | None = None
+    publish_date: str | None = None
+
+@app.put("/book/{book_id}")
+def update_book(book_id: int, updated_book: Book):
+    for book in books:
+        if book["id"] == book_id:
+            book["title"] = updated_book.title
+            book["author"] = updated_book.author
+            return book
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
+
+
+@app.delete("/book/{book_id}")
+def delete_book(book_id: int):
+    for book in books:
+        if book["id"] == book_id:
+            books.remove(book)
+            return {"detail": "Book deleted successfully"}
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Book not found")
